80f5aa77152039b62c524eb0de3871bbc1d53764
Jae Yoon KIM <zaywalker@gmail.com>
2020-02-02 20:36:55 +0900

    Update get_installer.sh

--- get_installer.sh
+++ get_installer.sh
@@ -6,7 +6,8 @@ set -e
 chown www-data:www-data /var/www/html/
 
 # Get installer.php if there is no index.php
-[[ ! -f /var/www/html/index.php ]] && curl -o /var/www/html/installer.php https://chevereto.com/download/file/installer && chown www-data:www-data /var/www/html/inst
+[[ ! -f /var/www/html/index.php ]] && curl -o /var/www/html/installer.php https://chevereto.com/download/file/installer
+chown www-data:www-data /var/www/html/installer.php
 
 # Run apache2
 apache2-foreground
